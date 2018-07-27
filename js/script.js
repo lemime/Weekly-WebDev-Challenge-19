082b4a27cdd09d636f8cfd645a5c00a24b634f00
lemime <kam.kolenda@gmail.com>
2018-07-27 16:20:21 +0200

    Add carousel animation

--- js/script.js
+++ js/script.js
@@ -1,70 +1,77 @@
 class Carousel {
     constructor() {
-
+        this.customers = document.querySelector('.customers');
+        this.currentElement;
         this.elements = document.querySelectorAll('.customer');
         this.elementNumber = this.elements.length;
         for (let [index, element] of this.elements.entries()) {
             element.style.order = index;
         }
+        this.elements[0].parentElement.classList.add('animate');
     }
     previous() {
-        let currentElement = document.querySelector('.primary');
-
-        currentElement.classList.remove('primary');
-        currentElement.classList.add('small');
-        if (currentElement.nextElementSibling) {
-            currentElement.nextElementSibling.classList.add('primary');
-            currentElement.nextElementSibling.classList.remove('small');
+        this.currentElement = document.querySelector('.primary');
+        this.currentElement.classList.remove('primary');
+        this.currentElement.classList.add('small');
+        if (this.currentElement.nextElementSibling) {
+            this.currentElement.nextElementSibling.classList.add('primary');
+            this.currentElement.nextElementSibling.classList.remove('small');
         } else {
             this.elements[0].classList.add('primary');
             this.elements[0].classList.remove('small');
         }
-
         let index = 2;
         for (let element of this.elements) {
-            currentElement.style.order = index;
+            this.currentElement.style.order = index;
             index--;
             if (index == -1) {
                 index = this.elementNumber;
             }
-            currentElement.nextElementSibling ? currentElement = currentElement.nextElementSibling : currentElement = this.elements[0];
-            currentElement.style.order = index;
+            this.currentElement.previousElementSibling ? this.currentElement = this.currentElement.previousElementSibling : this.currentElement = this.elements[this.elementNumber - 1];
+            this.currentElement.style.order = index;
         }
     }
     next() {
-        let currentElement = document.querySelector('.primary');
-
-        currentElement.classList.remove('primary');
-        currentElement.classList.add('small');
-        if (currentElement.previousElementSibling) {
-            currentElement.previousElementSibling.classList.add('primary');
-            currentElement.previousElementSibling.classList.remove('small');
+        this.currentElement = document.querySelector('.primary');
+        this.currentElement.classList.remove('primary');
+        this.currentElement.classList.add('small');
+        if (this.currentElement.previousElementSibling) {
+            this.currentElement.previousElementSibling.classList.add('primary');
+            this.currentElement.previousElementSibling.classList.remove('small');
         } else {
             this.elements[this.elementNumber - 1].classList.add('primary');
             this.elements[this.elementNumber - 1].classList.remove('small');
         }
-
-
         let index = 2;
         for (let element of this.elements) {
-            currentElement.style.order = index;
+            this.currentElement.style.order = index;
             index++;
             index = index % this.elementNumber;
-            currentElement.nextElementSibling ? currentElement = currentElement.nextElementSibling : currentElement = this.elements[0];
-            currentElement.style.order = index;
+            this.currentElement.nextElementSibling ? this.currentElement = this.currentElement.nextElementSibling : this.currentElement = this.elements[0];
+            this.currentElement.style.order = index;
         }
     }
-}
+    animate() {
+        this.customers.classList.remove('animate');
+        this.customers.offsetWidth;
+        this.customers.classList.add('animate');
 
+
+    }
+}
 
 
 document.addEventListener("DOMContentLoaded", function () {
 
     let carousel = new Carousel();
     document.querySelector('.next').addEventListener('click', function () {
+        carousel.customers.classList.remove('is-reversing');
+        carousel.animate();
         carousel.next();
     });
     document.querySelector('.prev').addEventListener('click', function () {
+        carousel.customers.classList.add('is-reversing');
+        carousel.animate();
         carousel.previous();
     })
 
